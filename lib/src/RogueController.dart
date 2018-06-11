4265dfb1edc25c6e66c0bfd103aa89fe99fe332b
Ruben Maurer <rubenmaurer@live.de>
2018-06-11 10:11:38 +0200

    Add death screen

--- lib/src/RogueController.dart
+++ lib/src/RogueController.dart
@@ -59,6 +59,11 @@ class RogueController {
     view.backAboutButton.onClick.listen((e) {
       _switchMenu(view.mainMenu, view.about);
     });
+
+    /* GAME OVER EVENTS */
+    /*view.backFromDead.onClick.listen((e) {
+      _switchMenu(view.mainMenu, view.gameOver);
+    });*/
   }
 
   _renderLevel(int stage) {
@@ -239,6 +244,11 @@ class RogueController {
           _spawnPlayer(player.currentStage);
         }
       }
+
+      if (!player.isAlive) {
+        _switchMenu(view.gameOver, view.game);
+      }
+
       _switchMenu(view.fightEnd, view.fightingOptions);
     }
 
